3270d439c05ff106bd55b1310485920e69627942
Ckyre <ckyre.game@gmail.com>
2021-11-24 17:06:35 +0100

    Clean project

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,16 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './App.css';
 
-import Header from "components/Header";
-
-// Exercices
-import FirstComponentExercice from 'exercices/FirstComponent'
-import StylingComponentsExercice from 'exercices/StylingComponents'
-import SpotifyExercice from 'exercices/Spotify'
-import ContextsExercice from 'exercices/Contexts'
-
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Switch>
-        <Route exact path="/first-component">
-          <FirstComponentExercice />
-        </Route>
-        <Route exact path="/styling-components">
-          <StylingComponentsExercice />
-        </Route>
-        <Route exact path="/spotify">
-          <SpotifyExercice />
-        </Route>
-        <Route exact path="/contexts">
-          <ContextsExercice />
-        </Route>
         <Route exact path="/">
-          <FirstComponentExercice />
+          Root
+        </Route>
+        <Route path="/">
+          Erreur 404
         </Route>
       </Switch>
     </BrowserRouter>
