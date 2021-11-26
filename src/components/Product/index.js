0bed7dd48e8198d8f4e478c800540ac0100a0132
Ckyre <ckyre.game@gmail.com>
2021-11-26 12:53:50 +0100

    home page

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -14,21 +14,57 @@ import "./style.css";
 const Product = () => {
   return (
     <section className="product-list">
-      <div class="product">
-        <div className="padding">
-          <Title subtitle="Air Jordan 1" title="Acclimate"/>
+      {/* Header navigation links */}
+      <nav className="header-navigation">
+        <div className="links">
+          <Link to="/" className="active">Feed</Link>
+          <Link to="/">En stock</Link>
+          <Link to="/">A venir</Link>
         </div>
-        <Image src="https://picsum.photos/500/500" alt="chaussure" className="preview" />
-        <div className="padding actions">
-          <div className="share">
-            <CTALikeButton />
-            <CTAShareButton />
-          </div>
-          <CTARemindingButton />
-        </div>
-      </div>
-      <hr />
-      <Link to="/details"> Voir les détails</Link>
+        <button className="search-btn">Rechercher</button>
+      </nav>
+
+      {/* Products feed */}
+      <ul>
+        <li><div class="product">
+            {/* Head */}
+            <div className="padding">
+              <Title subtitle="Air Jordan 1" title="Acclimate"/>
+            </div>
+
+            {/* Preview image */}
+            <Link to="/details">
+              <Image src="https://picsum.photos/500/500" alt="chaussure" className="preview" />
+            </Link>
+
+            {/* Actions */}
+            <div className="padding actions">
+              <div className="share">
+                <CTALikeButton />
+                <CTAShareButton />
+              </div>
+              <CTARemindingButton />
+            </div>
+          </div></li>
+
+          <li><div class="product">
+            <div className="padding">
+              <Title subtitle="Air Jordan 1" title="Acclimate"/>
+            </div>
+
+            <Link to="/details">
+              <Image src="https://picsum.photos/500/500" alt="chaussure" className="preview" />
+            </Link>
+
+            <div className="padding actions">
+              <div className="share">
+                <CTALikeButton />
+                <CTAShareButton />
+              </div>
+              <CTARemindingButton />
+            </div>
+          </div></li>
+      </ul>
     </section>
   );
 };
